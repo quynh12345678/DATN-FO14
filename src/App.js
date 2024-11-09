14f27e1c73d3810a04c924d03795716d9e154a98
Quynh12345678 <phunhmanhquynh@gmail.com>
2024-11-09 16:56:19 +0700

    Home

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import PropTypes from "prop-types";
+import React, { useEffect, Suspense, lazy } from "react";
+import ScrollToTop from "./helpers/scroll-top";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ToastProvider } from "react-toast-notifications";
+import { multilanguage, loadLanguages } from "redux-multilanguage";
+import { connect } from "react-redux";
+import { BreadcrumbsProvider } from "react-breadcrumbs-dynamic";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+
+const HomeCakeShop = lazy(() => import("./pages/home/HomeCakeShop"));
+
+// other pages
+const NotFound = lazy(() => import("./pages/other/NotFound"));
+
+const App = (props) => {
+  useEffect(() => {
+    props.dispatch(
+      loadLanguages({
+        languages: {
+          en: require("./translations/english.json"),
+          fn: require("./translations/french.json"),
+          de: require("./translations/germany.json"),
+        },
+      })
+    );
+  });
+  return (
+    <ToastProvider placement="bottom-left">
+      <BreadcrumbsProvider>
+        <Router>
+          <ScrollToTop>
+            <Suspense
+              fallback={
+                <div className="flone-preloader-wrapper">
+                  <div className="flone-preloader">
+                    <span></span>
+                    <span></span>
+                  </div>
+                </div>
+              }
+            >
+              <Switch>
+                <Route
+                  exact
+                  path={process.env.PUBLIC_URL + "/"}
+                  component={HomeCakeShop}
+                />
+
+                {/* Homepages */}
+
+                <Route path={"/home"} component={HomeCakeShop} />
+
+                <Route
+                  path={process.env.PUBLIC_URL + "/not-found"}
+                  component={NotFound}
+                />
+
+                <Route exact component={NotFound} />
+              </Switch>
+            </Suspense>
+          </ScrollToTop>
+        </Router>
+      </BreadcrumbsProvider>
+    </ToastProvider>
+  );
+};
+
+App.propTypes = {
+  dispatch: PropTypes.func,
+};
+
+export default connect()(multilanguage(App));
